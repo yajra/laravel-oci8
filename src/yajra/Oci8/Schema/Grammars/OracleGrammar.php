54fc6303649e0d923b3c8def58637a2c49c14529
Arjay Angeles <aqangeles@gmail.com>
2014-01-22 11:13:30 +0800

    fix column with default value and is not null (ORA-00907: missing right parenthesis)

--- src/yajra/Oci8/Schema/Grammars/OracleGrammar.php
+++ src/yajra/Oci8/Schema/Grammars/OracleGrammar.php
@@ -577,7 +577,12 @@ class OracleGrammar extends \Illuminate\Database\Schema\Grammars\Grammar {
 	 */
 	protected function modifyNullable(Blueprint $blueprint, Fluent $column)
 	{
-		return $column->nullable ? ' null' : ' not null';
+		//return $column->nullable ? ' null' : ' not null';
+		$null = $column->nullable ? ' null' : ' not null';
+		if ( ! is_null($column->default) ) {
+			return " default ".$this->getDefaultValue($column->default) . $null;
+		}
+		return $null;
 	}
 
 	/**
@@ -589,10 +594,7 @@ class OracleGrammar extends \Illuminate\Database\Schema\Grammars\Grammar {
 	 */
 	protected function modifyDefault(Blueprint $blueprint, Fluent $column)
 	{
-		if ( ! is_null($column->default))
-		{
-			return " default ".$this->getDefaultValue($column->default)."";
-		}
+		return "";
 	}
 
 	/**
