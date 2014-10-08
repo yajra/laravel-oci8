307b14173d4c2f85536509d4d3add306a498b75b
Arjay Angeles <aqangeles@gmail.com>
2014-10-08 18:28:48 +0800

    update service provider and make it Laravel 5 ready

--- src/yajra/Oci8/Oci8ServiceProvider.php
+++ src/yajra/Oci8/Oci8ServiceProvider.php
@@ -1,6 +1,7 @@
 <?php namespace yajra\Oci8;
 
 use Illuminate\Support\ServiceProvider;
+use yajra\Oci8\Connectors\OracleConnector as Connector;
 use Config;
 
 class Oci8ServiceProvider extends ServiceProvider {
@@ -29,30 +30,28 @@ class Oci8ServiceProvider extends ServiceProvider {
 	 */
 	public function register()
 	{
-
 		//Extend the connections with pdo-via-oci8 drivers by using a yajra\pdo\oci8 connector
 		foreach(Config::get('database.connections') as $conn => $config)
 		{
-
 			//Only use configurations that feature a "pdo-via-oci8" or "oci8" or "oracle" driver
 			if(!isset($config['driver']) || !in_array($config['driver'], array('pdo-via-oci8','oci8', 'oracle')) )
 			{
 				continue;
 			}
 
-			//Create a connector
-	        $this->app['db']->extend($conn, function($config)
-	        {
-	            $connector = new Connectors\OracleConnector();
-	            $connection = $connector->connect($config);
-	            $db = new Oci8Connection($connection, $config["database"], $config["prefix"]);
-	            // set oracle date format to match PHP's date
-	            $db->setDateFormat('YYYY-MM-DD HH24:MI:SS');
-	            return $db;
-	        });
-
+			$this->app->resolving('db', function($db)
+			{
+				$db->extend('oracle', function($config)
+				{
+					$connector = new Connector();
+					$connection = $connector->connect($config);
+					$db = new Oci8Connection($connection, $config["database"], $config["prefix"]);
+		            // set oracle date format to match PHP's date
+					$db->setDateFormat('YYYY-MM-DD HH24:MI:SS');
+					return $db;
+				});
+			});
 		}
-
 	}
 
 	/**
