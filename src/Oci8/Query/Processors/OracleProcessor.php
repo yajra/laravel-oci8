481751399038dd873db85f8b53118579c5c93af9
Arjay Angeles <aqangeles@gmail.com>
2016-02-09 17:00:38 +0800

    Cast all object values to string.

--- src/Oci8/Query/Processors/OracleProcessor.php
+++ src/Oci8/Query/Processors/OracleProcessor.php
@@ -56,7 +56,7 @@ class OracleProcessor extends Processor
     {
         $count = count($values);
         for ($i = 0; $i < $count; $i++) {
-            if ($values[$i] instanceof \DateTime) {
+            if (is_object($values[$i])) {
                 $values[$i] = (string) $values[$i];
             }
             $type = $this->getPdoType($values[$i]);
