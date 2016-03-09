892230dafd12fe83e28443d7c964deed8ef6e33f
Arjay Angeles <aqangeles@gmail.com>
2016-03-09 15:45:52 +0800

    Remove API.

--- api/elementlist.js
+++
@@ -1,3 +0,0 @@
-
-var ApiGen = ApiGen || {};
-ApiGen.elements = [["f","config_path()"],["c","Yajra\\Oci8\\Connectors\\OracleConnector"],["c","Yajra\\Oci8\\Eloquent\\OracleEloquent"],["c","Yajra\\Oci8\\Oci8Connection"],["c","Yajra\\Oci8\\Oci8ServiceProvider"],["c","Yajra\\Oci8\\OracleReservedWords"],["c","Yajra\\Oci8\\Query\\Grammars\\OracleGrammar"],["c","Yajra\\Oci8\\Query\\OracleBuilder"],["c","Yajra\\Oci8\\Query\\Processors\\OracleProcessor"],["c","Yajra\\Oci8\\Schema\\Comment"],["c","Yajra\\Oci8\\Schema\\Grammars\\OracleGrammar"],["c","Yajra\\Oci8\\Schema\\OracleAutoIncrementHelper"],["c","Yajra\\Oci8\\Schema\\OracleBlueprint"],["c","Yajra\\Oci8\\Schema\\OracleBuilder"],["c","Yajra\\Oci8\\Schema\\Sequence"],["c","Yajra\\Oci8\\Schema\\Trigger"]];
