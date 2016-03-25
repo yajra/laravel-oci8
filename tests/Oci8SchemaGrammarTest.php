d086a9034524ca4cb6b3bfb9f25d1f86b908afba
Maxime François <mfrancois@big-boss-studio.com>
2016-03-25 13:26:53 +0100

    Fix unite testing

--- tests/Oci8SchemaGrammarTest.php
+++ tests/Oci8SchemaGrammarTest.php
@@ -263,7 +263,7 @@ class Oci8SchemaGrammarTest extends PHPUnit_Framework_TestCase
     public function testCompileTableExistsMethod()
     {
         $grammar  = $this->getGrammar();
-        $expected = 'select * from user_tables where upper(table_name) = upper(?)';
+        $expected = 'select * from all_tables where upper(table_name) = upper(?)';
         $sql      = $grammar->compileTableExists();
         $this->assertEquals($expected, $sql);
     }
@@ -271,7 +271,7 @@ class Oci8SchemaGrammarTest extends PHPUnit_Framework_TestCase
     public function testCompileColumnExistsMethod()
     {
         $grammar  = $this->getGrammar();
-        $expected = 'select column_name from user_tab_columns where table_name = upper(\'test_table\')';
+        $expected = 'select column_name from all_tab_cols where table_name = upper(\'test_table\')';
         $sql      = $grammar->compileColumnExists("test_table");
         $this->assertEquals($expected, $sql);
     }
