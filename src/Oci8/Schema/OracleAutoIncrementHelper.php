bdc8911680d128bd30f7f07d53142795204970be
Arjay Angeles <aqangeles@gmail.com>
2016-05-18 12:21:34 +0800

    Fix fetching of table's primary key.

--- src/Oci8/Schema/OracleAutoIncrementHelper.php
+++ src/Oci8/Schema/OracleAutoIncrementHelper.php
@@ -138,7 +138,7 @@ class OracleAutoIncrementHelper
         $data = $this->connection->selectOne("
             SELECT cols.column_name
             FROM all_constraints cons, all_cons_columns cols
-            WHERE cols.table_name = upper('{$table}')
+            WHERE upper(cols.table_name) = upper('{$table}')
                 AND cons.constraint_type = 'P'
                 AND cons.constraint_name = cols.constraint_name
                 AND cons.owner = cols.owner
