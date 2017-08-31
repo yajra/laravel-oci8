ada0a8b63b11011994b2bce3a9061d9ca6c32b2c
Arjay Angeles <aqangeles@gmail.com>
2017-08-31 11:20:59 +0800

    Fix test.

--- tests/SequenceTest.php
+++ tests/SequenceTest.php
@@ -15,6 +15,7 @@ class SequenceTest extends PHPUnit_Framework_TestCase
     {
         $connection = $this->getConnection();
         $sequence   = new Sequence($connection);
+        $connection->shouldReceive('getConfig')->andReturn('');
         $connection->shouldReceive('statement')->andReturn(true);
         $success = $sequence->create('users_id_seq');
         $this->assertEquals(true, $success);
