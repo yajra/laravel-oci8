2740b2b720e0af70adf698d8e35d7f830b57d146
Arjay Angeles <aqangeles@gmail.com>
2017-09-13 11:22:47 +0800

    Fix cs.

--- tests/Functional/ProceduresAndFunctionsTest.php
+++ tests/Functional/ProceduresAndFunctionsTest.php
@@ -1,15 +1,64 @@
 <?php
 
-use Yajra\Oci8\Oci8Connection;
+use Illuminate\Database\Capsule\Manager as Capsule;
 use Illuminate\Database\Schema\Blueprint;
 use Yajra\Oci8\Connectors\OracleConnector;
-use Illuminate\Database\Capsule\Manager as Capsule;
+use Yajra\Oci8\Oci8Connection;
 
 class ProceduresAndFunctionsTest extends PHPUnit_Framework_TestCase
 {
     public function testConnection()
     {
         $this->assertInstanceOf(Oci8Connection::class, $this->createConnection());
+    }
+
+    /**
+     * @return Oci8Connection
+     */
+    private function createConnection()
+    {
+        $capsule = new Capsule;
+
+        $manager = $capsule->getDatabaseManager();
+        $manager->extend('oracle', function ($config) {
+            $connector  = new OracleConnector();
+            $connection = $connector->connect($config);
+            $db         = new Oci8Connection($connection, $config['database'], $config['prefix']);
+
+            // set oracle session variables
+            $sessionVars = [
+                'NLS_TIME_FORMAT'         => 'HH24:MI:SS',
+                'NLS_DATE_FORMAT'         => 'YYYY-MM-DD HH24:MI:SS',
+                'NLS_TIMESTAMP_FORMAT'    => 'YYYY-MM-DD HH24:MI:SS',
+                'NLS_TIMESTAMP_TZ_FORMAT' => 'YYYY-MM-DD HH24:MI:SS TZH:TZM',
+                'NLS_NUMERIC_CHARACTERS'  => '.,',
+            ];
+
+            // Like Postgres, Oracle allows the concept of "schema"
+            if (isset($config['schema'])) {
+                $sessionVars['CURRENT_SCHEMA'] = $config['schema'];
+            }
+
+            $db->setSessionVars($sessionVars);
+
+            return $db;
+        });
+
+        $capsule->addConnection([
+            'driver'       => 'oracle',
+            'host'         => 'localhost',
+            'database'     => 'xe',
+            'service_name' => 'xe',
+            'username'     => 'system',
+            'password'     => 'oracle',
+            'prefix'       => '',
+            'port'         => 49161,
+        ]);
+
+        // Make this Capsule instance available globally via static methods... (optional)
+        $capsule->setAsGlobal();
+
+        return $capsule->getConnection();
     }
 
     public function testProcedureWithNumbers()
@@ -34,7 +83,7 @@ class ProceduresAndFunctionsTest extends PHPUnit_Framework_TestCase
             'p1' => $input,
             'p2' => [
                 'value' => &$output,
-                'type'  => PDO::PARAM_INT|PDO::PARAM_INPUT_OUTPUT,
+                'type'  => PDO::PARAM_INT | PDO::PARAM_INPUT_OUTPUT,
             ],
         ];
 
@@ -69,7 +118,7 @@ class ProceduresAndFunctionsTest extends PHPUnit_Framework_TestCase
             'p2' => $last,
             'p3' => [
                 'value' => &$output,
-                'type'  => PDO::PARAM_STR|PDO::PARAM_INPUT_OUTPUT,
+                'type'  => PDO::PARAM_STR | PDO::PARAM_INPUT_OUTPUT,
             ],
         ];
 
@@ -143,54 +192,5 @@ class ProceduresAndFunctionsTest extends PHPUnit_Framework_TestCase
         $result = (int) $result;
 
         $this->assertSame($first + 2, $result);
-    }
-
-    /**
-     * @return Oci8Connection
-     */
-    private function createConnection()
-    {
-        $capsule = new Capsule;
-
-        $manager = $capsule->getDatabaseManager();
-        $manager->extend('oracle', function ($config) {
-            $connector  = new OracleConnector();
-            $connection = $connector->connect($config);
-            $db         = new Oci8Connection($connection, $config['database'], $config['prefix']);
-
-            // set oracle session variables
-            $sessionVars = [
-                'NLS_TIME_FORMAT'         => 'HH24:MI:SS',
-                'NLS_DATE_FORMAT'         => 'YYYY-MM-DD HH24:MI:SS',
-                'NLS_TIMESTAMP_FORMAT'    => 'YYYY-MM-DD HH24:MI:SS',
-                'NLS_TIMESTAMP_TZ_FORMAT' => 'YYYY-MM-DD HH24:MI:SS TZH:TZM',
-                'NLS_NUMERIC_CHARACTERS'  => '.,',
-            ];
-
-            // Like Postgres, Oracle allows the concept of "schema"
-            if (isset($config['schema'])) {
-                $sessionVars['CURRENT_SCHEMA'] = $config['schema'];
-            }
-
-            $db->setSessionVars($sessionVars);
-
-            return $db;
-        });
-
-        $capsule->addConnection([
-            'driver'       => 'oracle',
-            'host'         => 'localhost',
-            'database'     => 'xe',
-            'service_name' => 'xe',
-            'username'     => 'system',
-            'password'     => 'oracle',
-            'prefix'       => '',
-            'port'         => 49161,
-        ]);
-
-        // Make this Capsule instance available globally via static methods... (optional)
-        $capsule->setAsGlobal();
-
-        return $capsule->getConnection();
     }
 }
