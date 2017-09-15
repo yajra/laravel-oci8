d32037c4d0c3acf5b8364fdd202e3b91378a5da6
wingman <wingman@ip-home.net>
2017-09-15 15:39:34 +0300

    avoid wrap() stringify integers

--- src/Oci8/Query/Grammars/OracleGrammar.php
+++ src/Oci8/Query/Grammars/OracleGrammar.php
@@ -156,6 +156,24 @@ class OracleGrammar extends Grammar
         return ['truncate table ' . $this->wrapTable($query->from) => []];
     }
 
+
+    /**
+    * Wrap a value in keyword identifiers.
+    * 
+    * Override due to laravel's stringify integers.
+    * 
+    * @param  \Illuminate\Database\Query\Expression|string  $value
+    * @param  bool    $prefixAlias
+    * @return string
+    */
+    public function wrap($value, $prefixAlias = false)
+    {
+        if( is_integer($value) || is_float($value) )
+            return $value;
+
+        return parent::wrap($value, $prefixAlias);
+    }
+
     /**
      * Wrap a table in keyword identifiers.
      *
