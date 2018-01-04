e6532382ff0887344c321cd4bf0725c9c665c4db
Arjay Angeles <aqangeles@gmail.com>
2018-01-04 13:15:12 +0800

    Fix php7+ oci8 installation.

--- oci8.sh
+++ oci8.sh
@@ -20,14 +20,12 @@ sudo ln -s /opt/oracle/instantclient/libocci.so.12.1 /opt/oracle/instantclient/l
 
 sudo pecl channel-update pecl.php.net
 
-# php 7.0/7.1 install
-if [ $(phpenv version-name) == "7.1" ] || [ $(phpenv version-name) == "7.0" ]; then
-    echo 'instantclient,/opt/oracle/instantclient' | pecl install oci8
-fi
-
 # php 5.* install
 if [ $(phpenv version-name) == "5.6" ]; then
     echo 'instantclient,/opt/oracle/instantclient' | pecl install oci8-2.0.10
+else
+    # php 7.0/7.1/7.2 install
+    echo 'instantclient,/opt/oracle/instantclient' | pecl install oci8
 fi
 
 # run oracle db via docker
