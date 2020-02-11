e23781eee6e5339a477991b54c30074fbe3f4136
Arjay Q. Angeles <aqangeles@gmail.com>
2020-02-11 11:50:18 +0800

    Fix binding of values.

--- src/Oci8/Oci8Connection.php
+++ src/Oci8/Oci8Connection.php
@@ -341,6 +341,23 @@ class Oci8Connection extends Connection
     }
 
     /**
+     * Bind values to their parameters in the given statement.
+     *
+     * @param  \PDOStatement  $statement
+     * @param  array  $bindings
+     * @return void
+     */
+    public function bindValues($statement, $bindings)
+    {
+        foreach ($bindings as $key => $value) {
+            $statement->bindValue(
+                is_string($key) ? $key : $key + 1, $value,
+                is_int($value) || is_null($value) ? PDO::PARAM_INT : PDO::PARAM_STR
+            );
+        }
+    }
+
+    /**
      * Get the default query grammar instance.
      *
      * @return \Illuminate\Database\Grammar|\Yajra\Oci8\Query\Grammars\OracleGrammar
