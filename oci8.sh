bfb18f4d4b2fd4d1721d23fdcc9a6c70f118288b
Arjay Q. Angeles <aqangeles@gmail.com>
2020-03-04 16:12:03 +0800

    rpm

--- oci8.sh
+++ oci8.sh
@@ -7,16 +7,21 @@ sudo apt-get -y install -qq build-essential unzip wget libaio1
 # install oci8 libs & extension
 sudo mkdir -p /opt/oracle
 
-wget https://github.com/bumpx/oracle-instantclient/raw/master/instantclient-basic-linux.x64-12.1.0.2.0.zip
-wget https://github.com/bumpx/oracle-instantclient/raw/master/instantclient-sdk-linux.x64-12.1.0.2.0.zip
+wget https://github.com/bumpx/oracle-instantclient/raw/master/oracle-instantclient18.3-basic-18.3.0.0.0-1.x86_64.rpm
+wget https://github.com/bumpx/oracle-instantclient/raw/master/oracle-instantclient18.3-devel-18.3.0.0.0-1.x86_64.rpm
 
-sudo unzip -o ./instantclient-basic-linux.x64-12.1.0.2.0.zip -d /opt/oracle
-sudo unzip -o ./instantclient-sdk-linux.x64-12.1.0.2.0.zip -d /opt/oracle
+sudo rpm -ivh ./oracle-instantclient18.3-basic-18.3.0.0.0-1.x86_64.rpm
+sudo rpm -ivh ./oracle-instantclient18.3-devel-18.3.0.0.0-1.x86_64.rpm
 
-sudo ln -s /opt/oracle/instantclient/sqlplus /usr/bin/sqlplus
-sudo ln -s /opt/oracle/instantclient_12_1 /opt/oracle/instantclient
-sudo ln -s /opt/oracle/instantclient/libclntsh.so.12.1 /opt/oracle/instantclient/libclntsh.so
-sudo ln -s /opt/oracle/instantclient/libocci.so.12.1 /opt/oracle/instantclient/libocci.so
+# wget https://github.com/bumpx/oracle-instantclient/raw/master/instantclient-basic-linux.x64-12.1.0.2.0.zip
+# wget https://github.com/bumpx/oracle-instantclient/raw/master/instantclient-sdk-linux.x64-12.1.0.2.0.zip
+# sudo unzip -o ./instantclient-basic-linux.x64-12.1.0.2.0.zip -d /opt/oracle
+# sudo unzip -o ./instantclient-sdk-linux.x64-12.1.0.2.0.zip -d /opt/oracle
+
+# sudo ln -s /opt/oracle/instantclient/sqlplus /usr/bin/sqlplus
+# sudo ln -s /opt/oracle/instantclient_12_1 /opt/oracle/instantclient
+# sudo ln -s /opt/oracle/instantclient/libclntsh.so.12.1 /opt/oracle/instantclient/libclntsh.so
+# sudo ln -s /opt/oracle/instantclient/libocci.so.12.1 /opt/oracle/instantclient/libocci.so
 
 # sudo sh -c "echo '/opt/oracle/instantclient' >> /etc/ld.so.conf"
 # sudo ldconfig
