f4c8d19ea924f42175ad224b1fe143394a165923
Arjay Q. Angeles <aqangeles@gmail.com>
2020-12-07 00:04:28 +0800

    Fix binding values.

--- tests/Functional/QueryBuilderTest.php
+++ tests/Functional/QueryBuilderTest.php
@@ -3,7 +3,6 @@
 namespace Yajra\Oci8\Tests\Functional;
 
 use Yajra\Oci8\Tests\TestCase;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class QueryBuilderTest extends TestCase
@@ -14,12 +13,15 @@ class QueryBuilderTest extends TestCase
     public function it_can_perform_bulk_inserts()
     {
         $data = [
-            ['name' => 'Foo', 'job_id' => 0],
+            ['name' => 'Foo', 'job_id' => null],
             ['name' => 'Bar', 'job_id' => 1],
             ['name' => 'Test', 'job_id' => 2],
+            ['name' => null, 'job_id' => 4],
             ['name' => null, 'job_id' => null],
         ];
 
         $this->getConnection()->table('jobs')->insert($data);
+
+        $this->assertDatabaseCount('jobs', 5);
     }
 }
