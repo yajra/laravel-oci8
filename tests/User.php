90177774beafd72a0f1a7c82da31db8e358f2af4
Arjay Q. Angeles <aqangeles@gmail.com>
2020-12-07 00:17:14 +0800

    Add model insert tests.

--- tests/User.php
+++ tests/User.php
@@ -4,6 +4,11 @@ namespace Yajra\Oci8\Tests;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int id
+ * @property string name
+ * @property string email
+ */
 class User extends Model
 {
     protected $guarded = [];
