d63fe7c1eac593c1b8a2b88e62dba577fd9aa94b
Arjay Q. Angeles <aqangeles@gmail.com>
2020-12-06 15:08:17 +0800

    Fix unique and exists case insensitive validation.

--- src/Oci8/Validation/Oci8DatabasePresenceVerifier.php
+++ src/Oci8/Validation/Oci8DatabasePresenceVerifier.php
@@ -2,6 +2,58 @@
 
 namespace Yajra\Oci8\Validation;
 
-class Oci8DatabasePresenceVerifier
+use Yajra\Oci8\Oci8Connection;
+use Illuminate\Validation\DatabasePresenceVerifier;
+
+class Oci8DatabasePresenceVerifier extends DatabasePresenceVerifier
 {
+    /**
+     * Count the number of objects in a collection having the given value.
+     *
+     * @param string $collection
+     * @param string $column
+     * @param string $value
+     * @param int|null $excludeId
+     * @param string|null $idColumn
+     * @param array $extra
+     * @return int
+     */
+    public function getCount($collection, $column, $value, $excludeId = null, $idColumn = null, array $extra = [])
+    {
+        $connection = $this->table($collection)->getConnection();
+
+        if (! $connection instanceof Oci8Connection) {
+            return parent::getCount($collection, $column, $value, $excludeId, $idColumn, $extra);
+        }
+
+        $connection->setSessionVars(['NLS_COMP' => 'LINGUISTIC', 'NLS_SORT' => 'BINARY_CI']);
+        $count = parent::getCount($collection, $column, $value, $excludeId, $idColumn, $extra);
+        $connection->setSessionVars(['NLS_COMP' => 'BINARY', 'NLS_SORT' => 'BINARY']);
+
+        return $count;
+    }
+
+    /**
+     * Count the number of objects in a collection with the given values.
+     *
+     * @param string $collection
+     * @param string $column
+     * @param array $values
+     * @param array $extra
+     * @return int
+     */
+    public function getMultiCount($collection, $column, array $values, array $extra = [])
+    {
+        $connection = $this->table($collection)->getConnection();
+
+        if (! $connection instanceof Oci8Connection) {
+            return parent::getMultiCount($collection, $column, $values, $extra);
+        }
+
+        $connection->setSessionVars(['NLS_COMP' => 'LINGUISTIC', 'NLS_SORT' => 'BINARY_CI']);
+        $count = parent::getMultiCount($collection, $column, $values, $extra);
+        $connection->setSessionVars(['NLS_COMP' => 'BINARY', 'NLS_SORT' => 'BINARY']);
+
+        return $count;
+    }
 }
