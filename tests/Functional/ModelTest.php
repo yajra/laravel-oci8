36983e5015fdb4c4a333ca3503f68de90f064115
Arjay Angeles <yajra@users.noreply.github.com>
2020-12-06 11:59:36 +0000

    Apply fixes from StyleCI

--- tests/Functional/ModelTest.php
+++ tests/Functional/ModelTest.php
@@ -2,9 +2,9 @@
 
 namespace Yajra\Oci8\Tests\Functional;
 
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Yajra\Oci8\Tests\TestCase;
 use Yajra\Oci8\Tests\UserWithGuardedProperty;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ModelTest extends TestCase
 {
