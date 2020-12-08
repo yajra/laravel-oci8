de5e935c1935aed1c18b4d6c2fd70c7e45147c3d
Arjay Q. Angeles <aqangeles@gmail.com>
2020-12-08 18:50:26 +0800

    Use separate oci8 script for php8.

--- oci8.sh
+++ oci8.sh
@@ -7,22 +7,23 @@ sudo apt-get -y install -qq build-essential unzip wget libaio1
 # install oci8 libs & extension
 sudo mkdir -p /opt/oracle
 
-wget https://download.oracle.com/otn_software/linux/instantclient/199000/instantclient-basic-linux.x64-19.9.0.0.0dbru.zip
-wget https://download.oracle.com/otn_software/linux/instantclient/199000/instantclient-sdk-linux.x64-19.9.0.0.0dbru.zip
+wget https://github.com/bumpx/oracle-instantclient/raw/master/instantclient-basic-linux.x64-12.1.0.2.0.zip
+wget https://github.com/bumpx/oracle-instantclient/raw/master/instantclient-sdk-linux.x64-12.1.0.2.0.zip
 
-sudo unzip -o ./instantclient-basic-linux.x64-19.9.0.0.0dbru.zip -d /opt/oracle
-sudo unzip -o ./instantclient-sdk-linux.x64-19.9.0.0.0dbru.zip -d /opt/oracle
+sudo unzip -o ./instantclient-basic-linux.x64-12.1.0.2.0.zip -d /opt/oracle
+sudo unzip -o ./instantclient-sdk-linux.x64-12.1.0.2.0.zip -d /opt/oracle
 
 sudo ln -s /opt/oracle/instantclient/sqlplus /usr/bin/sqlplus
-sudo ln -s /opt/oracle/instantclient_19_9 /opt/oracle/instantclient
+sudo ln -s /opt/oracle/instantclient_12_1 /opt/oracle/instantclient
+sudo ln -s /opt/oracle/instantclient/libclntsh.so.12.1 /opt/oracle/instantclient/libclntsh.so
+sudo ln -s /opt/oracle/instantclient/libocci.so.12.1 /opt/oracle/instantclient/libocci.so
 
-PHPVersion=$(php --version | tail -r | tail -n 1 | cut -d " " -f 2 | cut -c 1,3)
-if [ $(echo " $PHPVersion <= 80" | bc) -eq 1 ]; then
-    sudo sh -c "echo 'instantclient,/opt/oracle/instantclient' | pecl install oci8-2.2.0"
-else
-    sudo sh -c "echo 'instantclient,/opt/oracle/instantclient' | pecl install oci8"
-fi
+sudo pecl channel-update pecl.php.net
 
-# setup ld library path
-sudo sh -c "echo '/opt/oracle/instantclient' >> /etc/ld.so.conf"
-sudo ldconfig
+echo 'instantclient,/opt/oracle/instantclient' | pecl install oci8
+
+# run oracle db via docker
+docker run -d -p 49160:22 -p 49161:1521 deepdiver/docker-oracle-xe-11g
+
+# wait for start
+sleep 40
