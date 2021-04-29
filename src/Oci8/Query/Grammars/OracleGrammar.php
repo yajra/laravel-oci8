dc2fdda0a585ac697847326af223bc78fbd28e87
Nathan <nathanfeitoza@outlook.com.br>
2021-04-28 23:28:09 -0300

    fix: fixing limit offset error in oracle 11g

--- src/Oci8/Query/Grammars/OracleGrammar.php
+++ src/Oci8/Query/Grammars/OracleGrammar.php
@@ -158,15 +158,15 @@ class OracleGrammar extends Grammar
         if ($query->limit == 1 && is_null($query->offset)) {
             return "select * from ({$sql}) where rownum {$constraint}";
         }
-
-        if (! is_null($query->limit && ! is_null($query->offset))) {
-            $start  = $query->offset + 1;
-            $finish = $query->offset + $query->limit;
-
-            return "select t2.* from ( select rownum AS \"rn\", t1.* from ({$sql}) t1 where rownum <= {$finish}) t2 where t2.\"rn\" >= {$start}";
-        }
-
-        return "select t2.* from ( select rownum AS \"rn\", t1.* from ({$sql}) t1 ) t2 where t2.\"rn\" {$constraint}";
+        
+        return "select
+                t2.*
+            from (
+                select rownum AS \"rn\", t1.* from ({$sql}) t1 
+            ) t2 
+            where 
+                t2.\"rn\" {$constraint}
+        ";
     }
 
     /**
