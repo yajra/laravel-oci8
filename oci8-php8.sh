791e9f753b473a7f61129f180724503e64d9ef2e
Arjay Angeles <aqangeles@gmail.com>
2021-12-22 10:46:45 +0800

    Set oci8 version for PHP8

--- oci8-php8.sh
+++ oci8-php8.sh
@@ -16,7 +16,7 @@ sudo unzip -o ./instantclient-sdk-linux.x64-19.9.0.0.0dbru.zip -d /opt/oracle
 sudo ln -s /opt/oracle/instantclient/sqlplus /usr/bin/sqlplus
 sudo ln -s /opt/oracle/instantclient_19_9 /opt/oracle/instantclient
 
-sudo sh -c "echo 'instantclient,/opt/oracle/instantclient' | pecl install oci8"
+sudo sh -c "echo 'instantclient,/opt/oracle/instantclient' | pecl install oci8-3.0.1"
 
 # setup ld library path
 sudo sh -c "echo '/opt/oracle/instantclient' >> /etc/ld.so.conf"
