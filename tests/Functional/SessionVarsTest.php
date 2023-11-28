1b5d0f895576f0f47444d2a41d861c9a66c8a1a8
Jose Rodriguez <jose.rodriguez@zecovery.com>
2023-11-28 11:22:10 +0100

    test: NLS_TIMESTAMP_FORMAT

---
+++ tests/Functional/SessionVarsTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Yajra\Oci8\Tests\Functional;
+
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Yajra\Oci8\Query\Grammars\OracleGrammar;
+use Yajra\Oci8\Query\OracleBuilder as Builder;
+use Yajra\Oci8\Query\Processors\OracleProcessor;
+use Yajra\Oci8\Tests\TestCase;
+
+class SessionVarsTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /**
+     * Configure custom sessionVars
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        parent::getEnvironmentSetUp($app);
+
+        $app['config']->set('database.connections.oracle.sessionVars', [
+            // Adds microseconds
+            'NLS_TIMESTAMP_FORMAT' => 'YYYY-MM-DD HH24:MI:SS.FF6',
+        ]);
+    }
+
+    /** @test */
+    public function it_can_redefine_timestamp_format()
+    {
+        $this->getConnection()->table('users')->truncate();
+        $data = ['id' => 2, 'name' => 'Foo', 'email' => 'foo@example.com', 'created_at' => '2023-11-28 13:14:15.123456'];
+
+        $this->getConnection()->table('users')->insert($data);
+
+        $this->assertDatabaseCount('users', 1);
+        $user = $this->getConnection()->table('users')->first();
+        $this->assertSame('2023-11-28 13:14:15.123456', $user->created_at);
+    }
+}
