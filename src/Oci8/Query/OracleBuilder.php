fc52a8a575ae51dc7bec25ea6372d545f2c5f583
Arjay Angeles <aqangeles@gmail.com>
2024-02-08 02:40:48 +0800

    fix: use transaction when in lock mode

--- src/Oci8/Query/OracleBuilder.php
+++ src/Oci8/Query/OracleBuilder.php
@@ -142,6 +142,24 @@ class OracleBuilder extends Builder
     }
 
     /**
+     * Run the query as a "select" statement against the connection.
+     *
+     * @return array
+     */
+    protected function runSelect()
+    {
+        if (isset($this->lock)) {
+            $this->connection->beginTransaction();
+            $result = $this->connection->select($this->toSql(), $this->getBindings(), ! $this->useWritePdo);
+            $this->connection->commit();
+
+            return $result;
+        }
+
+        return $this->connection->select($this->toSql(), $this->getBindings(), ! $this->useWritePdo);
+    }
+
+    /**
      * Set the table which the query is targeting.
      *
      * @param  \Closure|\Illuminate\Database\Query\Builder|string  $table
