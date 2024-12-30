2bc9a506124bc963e0ef3df056d43f4dfab1505c
Geraldo Magalhães <geraldo.santos.magalhaes@gmail.com>
2024-12-30 20:32:54 -0300

    Ignores tables that are secondary objects created by the ODCIIndexCreate method of the Oracle Data Cartridge

--- src/Oci8/Schema/Grammars/OracleGrammar.php
+++ src/Oci8/Schema/Grammars/OracleGrammar.php
@@ -377,7 +377,7 @@ class OracleGrammar extends Grammar
     public function compileDropAllTables()
     {
         return 'BEGIN
-            FOR c IN (SELECT table_name FROM user_tables) LOOP
+            FOR c IN (SELECT table_name FROM user_tables WHERE secondary = \'N\') LOOP
             EXECUTE IMMEDIATE (\'DROP TABLE "\' || c.table_name || \'" CASCADE CONSTRAINTS\');
             END LOOP;
 
