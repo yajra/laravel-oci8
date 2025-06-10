01035c1b56025dcac3c9982850d135f7b8b57688
Arjay Angeles <aqangeles@gmail.com>
2025-06-10 14:41:06 +0800

    test: add assertion

--- tests/Functional/ConnectionDateFormatTest.php
+++ tests/Functional/ConnectionDateFormatTest.php
@@ -11,6 +11,7 @@ class ConnectionDateFormatTest extends TestCase
     #[Test]
     public function it_can_set_the_date_format()
     {
+        /** @var \Yajra\Oci8\Oci8Connection $connection */
         $connection = $this->getConnection();
 
         $connection->setDateFormat('YYYY-MM-DD');
@@ -18,6 +19,11 @@ class ConnectionDateFormatTest extends TestCase
         $date = $connection->select('select sysdate from dual');
         $format = Carbon::now()->format('Y-m-d');
 
+        $this->assertSame($format, $date[0]->sysdate);
+
+        $connection->setDateFormat('MM/DD/YYYY');
+        $date = $connection->select('select sysdate from dual');
+        $format = Carbon::now()->format('m/d/Y');
         $this->assertSame($format, $date[0]->sysdate);
     }
 }
