e213eace26a80c5e4ef21415458c7cc1647b3214
Arjay Angeles <aqangeles@gmail.com>
2025-11-19 14:32:09 +0800

    ci: test db config

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -18,7 +18,7 @@ abstract class TestCase extends BaseTestCase
         $this->seedDatabase();
     }
 
-    protected function migrateDatabase()
+    protected function migrateDatabase(): void
     {
         /** @var \Illuminate\Database\Schema\Builder $schemaBuilder */
         $schemaBuilder = $this->app['db']->connection()->getSchemaBuilder();
@@ -56,7 +56,7 @@ abstract class TestCase extends BaseTestCase
         });
     }
 
-    protected function seedDatabase()
+    protected function seedDatabase(): void
     {
         collect(range(1, 20))->each(function ($i) {
             /** @var User $user */
@@ -72,16 +72,16 @@ abstract class TestCase extends BaseTestCase
      *
      * @param  \Illuminate\Foundation\Application  $app
      */
-    protected function getEnvironmentSetUp($app)
+    protected function getEnvironmentSetUp($app): void
     {
         $app['config']->set('app.debug', true);
         $app['config']->set('database.default', 'oracle');
         $app['config']->set('database.connections.oracle', [
             'driver' => 'oracle',
             'host' => 'localhost',
-            'database' => 'free',
-            'service_name' => 'free',
-            'username' => 'oraora',
+            'database' => 'xe',
+            'service_name' => 'xe',
+            'username' => 'system',
             'password' => 'oracle',
             'port' => 1521,
         ]);
